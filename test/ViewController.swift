1f86026a923c9554b7074d1d7bf9b7fd0cc9baed
McCabe <mccabe@wambl.com>
2017-11-03 14:21:23 -0700

    working

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     var seconds = 1
     var time = 0
     var maximum: Int = 0
+    var minimum: Double = 1
+    var maxval: Double = 0
     weak var timer: Timer?
     var startTime: Double = 0
     var elapsed: Double = 0
@@ -24,7 +26,7 @@ class ViewController: UIViewController {
     var dummyTimer = Timer()
     
     @IBAction func startButtonPressed(_ sender: UIButton) {
-        start()
+//        start()
         print ("start button pressed")
         
     }
@@ -32,39 +34,40 @@ class ViewController: UIViewController {
     
     
     func start(){
-       
-        print("start function initiated")
-        elapsed = 0
-        self.time = 0
-        startTime = Date().timeIntervalSinceReferenceDate
-        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true){
-        timer in
-        
-        self.time += 1
-        self.height.text = String(self.time)
-        print(self.time)
-            
+        if !isTimerRunning {
+            isTimerRunning = true
+            print("start function initiated")
+            time = 0
+            startTime = Date().timeIntervalSinceReferenceDate
+            timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true){
+                timer in
+                
+                self.time += 1
+                self.height.text = String(self.time)
+                print(self.time)
+            }
         }
     }
     func stop(){
+        if isTimerRunning {
+            isTimerRunning = false
+            timer?.invalidate()
+            
+            print ("elapsed", time)
+            if (time > maximum){
+                maximum = time
+            }
+            max.text = String(maximum)
+            print(maximum)
+            //        height.text = String(elapsed)
+        }
 //        elapsed = Date().timeIntervalSinceReferenceDate - startTime
 //        elapsed = round(Date().timeIntervalSinceReferenceDate - startTime)*100/100
-        
-        print ("elapsed",self.time)
-        if (self.time > maximum){
-            maximum = self.time
-        }
-        max.text = String(maximum)
-        print(maximum)
-//        height.text = String(elapsed)
-        timer?.invalidate()
-        
-       
     }
     
     @IBAction func stopButtonPressed(_ sender: UIButton) {
 //        isTimerRunning = false
-        stop()
+//        stop()
 
         
     }
@@ -89,6 +92,7 @@ class ViewController: UIViewController {
             
             
         var total : Double = 0
+        var accel : Double = 0
         if let manager = motionManager{
             print("we have motion manager")
             if manager.isDeviceMotionAvailable {
@@ -99,34 +103,54 @@ class ViewController: UIViewController {
                 manager.deviceMotionUpdateInterval = 0.1
                 manager.startDeviceMotionUpdates(to: myQ, withHandler: { (data: CMDeviceMotion?, error: Error? ) in
                     if let mydata = data {
-//
+//                            print ("x gravity:", mydata.userAcceleration.x)
+                       accel = abs(mydata.userAcceleration.x) + abs(mydata.userAcceleration.y) + abs(mydata.userAcceleration.z)
 //                        print ("x gravity:", mydata.gravity.x)
 //                          print ("y gravity:", mydata.gravity.y)
 //                          print ("z gravity:", mydata.gravity.z)
 //                        total = (abs(mydata.gravity.x) + abs(mydata.gravity.y) + abs(mydata.gravity.z))
-                        total = abs(mydata.gravity.x + mydata.gravity.y + mydata.gravity.z)
-                           print ("total: ",total)
-                        if (total > 0.6){
-                            self.status = false
+                        total = abs(mydata.gravity.x) + abs(mydata.gravity.y) + abs(mydata.gravity.z)
+//                           print ("total: ",total)
+                        
+                        if self.isTimerRunning {
+                            self.time += 1
+                            DispatchQueue.main.async {
+                                self.height.text = String(self.time)
+                            }
+                            print(self.time)
                         }
-                        else{
-                            self.status = true
+                        
+                        
+                        if (total > 1.4 && accel > 0.8){
+                            if !self.isTimerRunning {
+                                self.isTimerRunning = true
+                                print("start function initiated")
+                                self.time = 0
+                            }
                         }
-                        if(self.status == false){
-                            print("status",self.status)
-                            
-                            self.status = true
-                             print("status",self.status)
-                            
-                            self.start()
-
+                        if ( accel < 0.8 ){
+                            if self.isTimerRunning {
+                                self.isTimerRunning = false
+                                print("stop function initiated")
+                                if (self.time > self.maximum){
+                                    self.maximum = self.time
+                                }
+                                DispatchQueue.main.async {
+                                    self.max.text = String(self.maximum)
+                                }
+                                print(self.maximum)
+                            }
                         }
-                        if (self.status == true){
-                            
-                            self.status = false
-                            
-                            self.stop()
+                        
+                        if (total > self.maxval){
+                            self.maxval = total
                         }
+                        if (total < self.minimum){
+                            self.minimum = total
+                            print("self.maximum",self.maxval, "self.min", self.minimum)
+                        }
+                    
+                       
                         
                     }
                     if let myerror = error {
